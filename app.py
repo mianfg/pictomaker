ff1167e91df9d53cc1bc5b9ce1a87a913705d9b7
Javier Saez <jscoba@gmail.com>
2019-12-01 15:35:43 +0100

    Refactor some code to improve usability. Solves JSON generation problem

--- app.py
+++ app.py
@@ -7,12 +7,6 @@ from imagehandler import PictoImageHandler
 from interface import PictoInterface
 from language import PictoLanguage
 
-import io
-import os
-import uuid
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-import numpy as np
 
 app = Flask(__name__)
 app.secret_key = 's3cr3t'
@@ -21,19 +15,12 @@ app.debug = True
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index2.html')
 
 @app.route('/editor', methods=['GET'])
 def editor():
     return render_template('editor.html')
 
-@app.route('/results/<uuid>', methods=['GET'])
-def results(uuid):
-    title = 'Result'
-    data = get_file_content(uuid)
-    return render_template('layouts/results.html',
-                           title=title,
-                           data=data)
 
 @app.route('/textpost', methods = ['POST'])
 def post_text():
@@ -47,41 +34,11 @@ def post_text():
     return jsonify({'cards': cards_json})
     #return jsonify({'message': "OK"})
 
-@app.route('/postmethod', methods = ['POST'])
-def post_javascript_data():
-    jsdata = request.form['canvas_data']
-    unique_id = create_csv(jsdata)
-    params = { 'uuid' : unique_id }
-    return jsonify(params)
 
-@app.route('/plot/<imgdata>')
-def plot(imgdata):
-    data = [float(i) for i in imgdata.strip('[]').split(',')]
-    data = np.reshape(data, (200, 200))
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    axis.axis('off')
-    axis.imshow(data, interpolation='nearest')
-    canvas = FigureCanvas(fig)
-    output = io.BytesIO()
-    canvas.print_png(output)
-    response = make_response(output.getvalue())
-    response.mimetype = 'image/png'
-    return response
-
-def create_csv(text):
-    unique_id = str(uuid.uuid4())
-    with open('images/'+unique_id+'.csv', 'a') as file:
-        file.write(text[1:-1]+"\n")
-    return unique_id
-
-def get_file_content(uuid):
-    with open('images/'+uuid+'.csv', 'r') as file:
-        return file.read()
 
 if __name__ == '__main__':
     image_handler = PictoImageHandler(
-        font = "./resources/escolar_bold.ttf",
+        font = "./static/escolar_bold.ttf",
         text_size = 90,
         card_dimensions = (600,750),
         image_margin = 50
